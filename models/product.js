3f6ef22f42b742476d99183671501eb70d1fbed8
spkb <spkb@gmx.com>
2015-08-23 13:13:51 +0100

    schema's created

--- models/product.js
+++ models/product.js
@@ -1,7 +1,16 @@
 var mongoose = require('mongoose');
-var Product = mongoose.model('Product');
-var productSchema = new mongoose.Schema({
 
-})
+var Product = mongoose.model('Product');
+
+var productSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	quantity: String,
+	bestBefore: String,
+	available: Boolean,
+	userId: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
 var Product = mongoose.model('Product', productSchema);
+
 module.exports = Product;
--- models/transaction.js
+++ models/transaction.js
@@ -1,7 +1,17 @@
 var mongoose = require('mongoose');
-var Transaction = mongoose.model('Transaction');
-var transactionSchema = new mongoose.Schema({
 
+var Transaction = mongoose.model('Transaction');
+
+var transactionSchema = new mongoose.Schema({
+	giverId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	takerId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	availableTime: String,
+	meetingTime: Date,
+	location: String,
+	status: Boolean,
+	rating: Number
 })
+
 var Transaction = mongoose.model('Transaction', transactionSchema);
+
 module.exports = Transaction;
