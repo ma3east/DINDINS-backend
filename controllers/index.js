ff5b3f4a135e88b5a4320c1eb167e1b127323896
Andrea Mckenzie <ma3east@gmail.com>
2015-08-23 22:29:29 +0100

    made some changes to controllers - issue with prodcuts index not working when on prod controller but ok on index

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/users', require('./users'))
-router.use('/products', require('./products'))
-router.use('/transactions', require('./transactions'))
+router.use('/api', router);
+router.use('/api/users', require('./users'))
+router.use('/api/products', require('./products'))
+router.use('/api/transactions', require('./transactions'))
 
+//get api index
+router.get('/api', function(req, res) {
+  res.json({ message: 'This is a call to the DINDINS API index - are you running dangerously low on food!' });
+
+  // want to return a list of all the products!
+});
+
+//why does this route not work when on products page?
+router.get('/api/products', function(req, res, err) {
+
+  if (err)
+    console.log(err);
+
+  res.json({ message: 'This is the api products index.'});
+  //need to return all products
+});
 
 
 module.exports = router
