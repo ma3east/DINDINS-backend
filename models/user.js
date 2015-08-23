0547a7d5c30a765141b760a4dab76c917d685a1f
spkb <spkb@gmx.com>
2015-08-23 12:58:59 +0100

    user model ready

--- models/user.js
+++ models/user.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
-var userSchema = new mongoose.Schema({
 
+var User = mongoose.model('User');
+
+var userSchema = new mongoose.Schema({
+	username: String,
+	email: String,
+	location: String
 })
+
 var User = mongoose.model('User', userSchema);
+
 module.exports = User;
