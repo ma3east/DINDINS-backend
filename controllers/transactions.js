e492ecef51ee1914127e2cf817af076d8505407a
Andrea Mckenzie <ma3east@gmail.com>
2015-08-24 10:41:33 +0100

    fixing files

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+var Product = require('../models/product');
+var Transaction = require('../models/transaction');
+
 
 
 module.exports = router
