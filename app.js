d282976d0c8587f2273e515f0da8fa651ccf7501
Andrea Mckenzie <ma3east@gmail.com>
2015-08-24 09:29:15 +0100

    sending through changes

--- app.js
+++ app.js
@@ -8,17 +8,45 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 //var User = require('./models/user');
-//var Product = require('./models/product');
+var Product = require('./models/product');
 //var Transaction = require('./models/transaction');
 
 mongoose.connect('mongodb://localhost/dindins');
+
+app.use(require('./controllers'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-app.use(require('./controllers'));
 
+app.use('/api', router);
 
 app.listen(port);
 console.log("Dindin bells are ringing on port " + port);
+
+// var prod1 = new Product({
+
+//   name: "pasta",
+//   quantity: "2 packets"
+
+// })
+
+// prod1.save(function(err) {
+//   if (err) console.log(err, "nope something went wrong");
+//   console.log("success prod 1 saved!");
+  
+// });
+
+// var prod2 = new Product({
+
+//   name: "rice",
+//   quantity: "3 packets"
+
+// })
+
+// prod2.save(function(err) {
+//   if (err) console.log(err, "nope something went wrong");
+//   console.log("success prod 2 saved!");
+  
+// });
