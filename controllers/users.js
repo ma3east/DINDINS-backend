8cdcbe6c6003c474562505d5b894b052bcd8a8a1
Andrea Mckenzie <ma3east@gmail.com>
2015-08-25 14:04:53 +0100

    added bcrypt authentication for user login and password hashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var bcrypt = require('bcrypt');
 
 var User = require('../models/user');
 var Product = require('../models/product');
 var Transaction = require('../models/transaction');
 
-//get list of users (probably not needed but incase)
+//get list of users (probably not needed but incase) - WORKING
 router.get('/', function(req, res) {
   User.find(function(err, users) {
     if (err) {
@@ -15,6 +17,32 @@ router.get('/', function(req, res) {
     }
   })
 });
+
+//login a user - WORKING
+
+router.post('/login', function(req, res) {
+
+  var userParams = req.body;
+
+  User.findOne({ email: userParams.email }, function(err, user) {
+
+    user.authenticate(userParams.password, function(err, isMatch) {
+      if (err) {
+        throw err;
+      }
+
+      if (isMatch) {
+        console.log("valid credentials: " + "\n" + user);
+        res.json(user);  
+      } else {
+        console.log("error no match");
+        res.status(401).send( {message: "The credentials provided do not match to a registered user!"} );
+        
+      };
+    });
+  });
+});
+
 
 //find a single user - WORKING
 router.get('/:user_id', function(req, res){
@@ -28,18 +56,19 @@ router.get('/:user_id', function(req, res){
   });
 });
 
-// create new user - WORKING
+// create new user - WORKING (and password hashed)
 router.post('/', function(req, res) {
   var user = new User(req.body)
 
-  user.save(function(err) {
+  user.save(function(err, user) {
     if (err) {
-      res.send(err)
+      console.log(err);
+      res.status(401).send( {message: err.errmsg});
+    } else {
+      console.log('User added!');
+      res.json(user);
     } 
-    console.log('User added!');
-    res.json(user);
   });
-
 });
 
 // update a user - WORKING
@@ -82,6 +111,7 @@ router.delete('/:user_id', function(req, res) {
     }
   });
 });
+
 
 module.exports = router
 
